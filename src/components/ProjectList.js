43744b199608f263126732c3922523d9b50227ff
Stuart Forrester <stforrester@gmail.com>
2024-12-03 05:03:10 -0600

    adds dynamic rendering of ProjectItems in ProjectList component

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,10 +3,18 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+
+  const projectItemsList = projects.map((project) => {
+    return <ProjectItem key={project.id} name={project.name} about={project.about} technologies={project.technologies} ></ProjectItem>
+  });
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {/* render ProjectItem components here */}
+        {projectItemsList}
+      </div>
     </div>
   );
 }
